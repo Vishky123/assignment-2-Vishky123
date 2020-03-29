c7f4a2abf45f185893d346a76ee8bfba4256f1a0
Vishal Yadav <vishal.yadav_cs18@gla.ac.in>
2020-03-29 14:20:22 +0530

    Traversing the Tree.

--- src/problem2/main/TraversalTree.java
+++ src/problem2/main/TraversalTree.java
@@ -1,4 +1,34 @@
 package problem2.main;
+import problem1.node.TreeNode;
 
 public class TraversalTree {
+    public class Traversal {
+        void printPostorder(TreeNode node) {
+
+            if (node == null)
+
+                return;
+
+            printPostorder(node.getLeft());
+
+            printPostorder(node.getRight());
+
+            System.out.print(node.getData() + " ");
+
+        }
+
+        void printInorder(TreeNode node) {
+
+            if (node == null)
+
+                return;
+
+            printInorder(node.getLeft());
+
+            System.out.print(node.getData() + " ");
+
+            printInorder(node.getRight());
+
+        }
+    }
 }
