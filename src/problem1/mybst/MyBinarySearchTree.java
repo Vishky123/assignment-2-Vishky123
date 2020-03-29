f7d55ff00d45df6f8b0d400067481b0e127d17f1
Vishal Yadav <vishal.yadav_cs18@gla.ac.in>
2020-03-29 13:44:19 +0530

    create method to insert data to tree in left and right subtree.

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -5,7 +5,29 @@
  *  Time: 7:17 PM
  */
 package problem1.mybst;
+
+import problem1.node.TreeNode;
+
+import java.util.Queue;
+
 // to implement BinarySearchTree
 public class MyBinarySearchTree {
+    static Queue<TreeNode> var;
 
+    public TreeNode insert(TreeNode root, int value) {
+
+        if (root == null)
+
+            return new TreeNode(value);
+
+        else if (value > root.getData()) // value is greater. Should be inserted to right
+
+            root.setRight(insert(root.getRight(), value));
+
+        else // value is smaller should be inserted to left
+
+            root.setLeft(insert(root.getLeft(), value));
+
+        return root;
+    }
 }
