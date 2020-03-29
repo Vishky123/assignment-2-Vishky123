0e22838fd3f7459ccbdf12f4aba89b6b16b6302e
Vishal Yadav <vishal.yadav_cs18@gla.ac.in>
2020-03-29 15:00:17 +0530

    create getter setter and constructor for private variable.

--- src/problem5/student/Student.java
+++ src/problem5/student/Student.java
@@ -7,7 +7,27 @@
 package problem5.student;
 // to store student information and properties
 public class Student {
-    private int backlogCount;
-    private String studentName;
+    private int backlog;
+    private String name;
 
+    public Student(int backlog, String name) {
+        this.backlog = backlog;
+        this.name = name;
+    }
+
+    public int getBacklog() {
+        return backlog;
+    }
+
+    public void setBacklog(int backlog) {
+        this.backlog = backlog;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
