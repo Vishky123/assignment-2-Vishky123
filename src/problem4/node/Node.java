8a2053cebf83638b3f9e1e34920fe51f505d02c6
Vishal Yadav <vishal.yadav_cs18@gla.ac.in>
2020-03-29 14:51:30 +0530

    create getter setter and constructor for private variable.

--- src/problem4/node/Node.java
+++ src/problem4/node/Node.java
@@ -10,5 +10,22 @@ public class Node {
     public Node(TreeNode node) {
         this.node = node;
         this.next = null;
+
+    }
+
+    public TreeNode getNode() {
+        return node;
+    }
+
+    public void setNode(TreeNode node) {
+        this.node = node;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
     }
 }
