4b85608b80d4617ac48256513e2c552241c4bacd
Vishal Yadav <vishal.yadav_cs18@gla.ac.in>
2020-03-29 14:46:00 +0530

    init commit.

--- src/problem3/main/MyMain.java
+++ src/problem3/main/MyMain.java
@@ -5,7 +5,32 @@
  *  Time: 7:13 PM
  */
 package problem3.main;
+
+import problem3.Student.Student;
+import problem3.myqueue.MyPriorityQueue;
+
+import java.util.Scanner;
+
 // executable class
 // use problem5.student.Student class to create object of student
 public class MyMain {
+    public static void main(String[] args) {
+        MyPriorityQueue queue = new MyPriorityQueue();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the no of Student you want insert");
+        int value = sc.nextInt();
+        sc.nextLine();
+        for (int i = 0; i < value; i++) {
+            System.out.println("Enter the Student name");
+            String name = sc.nextLine();
+            System.out.println("Enter the Roll Number for the student");
+            int rollno = sc.nextInt();
+            Student student = new Student(name, rollno);
+            queue.enqueue(student);
+            sc.nextLine();
+        }
+        queue.show();
+
+    }
 }
+
