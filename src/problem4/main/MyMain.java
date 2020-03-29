023fcf8b6bf9ed508ee3e57d5f7cc6b100fc61db
Vishal Yadav <vishal.yadav_cs18@gla.ac.in>
2020-03-29 14:57:23 +0530

    complete my main method.

--- src/problem4/main/MyMain.java
+++ src/problem4/main/MyMain.java
@@ -5,7 +5,35 @@
  *  Time: 7:08 PM
  */
 package problem4.main;
+
+import problem1.mybst.MyBinarySearchTree;
+import problem1.node.TreeNode;
+import problem4.myqueue.MyQueue;
+
+import java.util.Scanner;
+
 // executable class
 // use problem1.mybst.MyBinarySearchTree class for creating binary tree
 public class MyMain {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        MyQueue queue = new MyQueue();
+        System.out.println("Enter the no of data you want to add ");
+        int data = sc.nextInt();
+        System.out.println("Enter the value");
+        MyBinarySearchTree child = new MyBinarySearchTree();
+
+        TreeNode root;
+
+        root = new TreeNode(data);
+        for (int i = 0; i < data; i++) {
+            child.insert(root, sc.nextInt());
+        }
+        queue.preOrder(root);
+        queue.queuePrint(queue);
+        System.out.println("Enter to find preorder Successor :");
+        queue.printSuccessor(sc.nextInt());
+
+    }
 }
+
