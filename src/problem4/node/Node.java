e1dcfb9e58abb457f690287c02eb7b092fb8b721
Vishal Yadav <vishal.yadav_cs18@gla.ac.in>
2020-03-29 14:50:04 +0530

    create two private variable TreeNode node and Node next.

---
+++ src/problem4/node/Node.java
@@ -0,0 +1,14 @@
+package problem4.node;
+
+import problem1.node.TreeNode;
+
+public class Node {
+    private TreeNode node;
+
+    private Node next;
+
+    public Node(TreeNode node) {
+        this.node = node;
+        this.next = null;
+    }
+}
