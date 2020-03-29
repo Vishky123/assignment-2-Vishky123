4644e5ca0a285dd904efe2a4803ab2df6798ca75
Vishal Yadav <vishal.yadav_cs18@gla.ac.in>
2020-03-29 14:02:29 +0530

    create object in main class name child.

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -7,5 +7,27 @@
 package problem1.main;
 // executable class
 
+import problem1.mybst.MyBinarySearchTree;
+import problem1.node.TreeNode;
+
+import java.util.Scanner;
+
 public class MyMain {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the no of data you want to add ");
+        int data = sc.nextInt();
+        System.out.println("Enter the value");
+        MyBinarySearchTree child = new MyBinarySearchTree();
+
+        TreeNode root;
+
+        root = new TreeNode(data);
+        for (int i = 0; i < data; i++) {
+            child.insert(root, sc.nextInt());
+        }
+
+
+    }
 }
+
